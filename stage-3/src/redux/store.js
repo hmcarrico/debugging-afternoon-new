806c1a58a023cca427846fefdbc1eefe2ddc7a25
Hunter Carrico <huntercarrico@Hunters-MacBook-Air.local>
2018-12-20 10:18:59 -0700

    finished

--- stage-3/src/redux/store.js
+++ stage-3/src/redux/store.js
@@ -1,4 +1,6 @@
 import reducer from './reducer';
+import {createStore} from 'redux';
+import {applyMiddleware} from 'redux';
 import reduxPromiseMiddleware from 'redux-promise-middleware';
 
 export default createStore(reducer, applyMiddleware(reduxPromiseMiddleware()));
